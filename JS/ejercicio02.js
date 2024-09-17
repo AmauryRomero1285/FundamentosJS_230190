8b575f28ea3bb1f976b4a5eba26775a191607b51
Amaury Romero Martínez <230190@utxicotepec.edu.mx>
2024-09-17 13:37:13 -0600

    Practica 03
    Creación del Archivo app.css

---
+++ JS/ejercicio02.js
@@ -0,0 +1,15 @@
+//Tipos de Datos en JS
+
+// 1.Undefined - (NUll)
+console.warn("--- Tipo de Dato UNDEFINED");
+let cliente;
+console.log (`El cliente es ${cliente}`);
+console.log(`El tipo de dato de la variable cliente es :${typeof(cliente)}`);
+
+cliente="Juan Romero";
+console.log (`El cliente es ${cliente}`);
+console.log(`El tipo de dato de la variable cliente es :${typeof(cliente)}`);
+
+cliente=19227;
+console.log (`El cliente es ${cliente}`);
+console.log(`El tipo de dato de la variable cliente es :${typeof(cliente)}`);
