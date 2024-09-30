81c252cc5e7928e864887d16f187c441c173f29c
Yáred Amaury Romero <230190@utxicotepec.edu.mx>
2024-09-30 11:54:46 -0600

    Práctica 06
    Creción de archivo ejercicio04.js y actualización del mismo

--- JS/ejercicio04.js
+++ JS/ejercicio04.js
@@ -1,99 +1,122 @@
-//Repaso de Arreglos
-//Estilización de los mensajes de salida
-const bg="linear-gradient(11deg,rgba(2,0,36,1) 0%, rgba(9,9,121,1) 33%, rgba(0,212,255,1) 86%)";
-const style_console =`background: ${bg};color:red; border-radius : 6px;padding:4px;font-sixe:1.0rem`
-
-//Personalización de salidas a consola
-console.warn("Práctica 06: Arreglos en JavaScript");
+// Repaso de Arreglos
 
 
-//Declaración de un arreglo
-//Para declarar un arreglo (array) de datos en javascript basta con agregar[], y dentro los datos que contendrá el arreglo.
 
-const mesesAnio=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+//Estilización de las salidas a consola
+const bg = "linear-gradient(11deg, rgba(199,0,36,1) 0%, rgba(255,129,121,1) 33%, rgba(255,191,26,1) 86%)";
+const style_console = `background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold`
+
+console.warn("Practica 06: Arreglos en Java Script")
 
 
-console.log("%c1.- Declaración de un arreglo (Array)",style_console);
+//Para declarar un arreglo (array) de datos en JavaScript basta con comenzar con agregar [], y dentro de los datos que contendra el arreglo 
+const mesesAnio=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+
+console.log("%c1.- Declaración de un arreglo (Array)", style_console);
+
 console.log(mesesAnio);
 console.table(mesesAnio);
 
-//Acceder a los valores de un arreglo de datos para acceder al valor solo basta enviar la posición del dato que queremos dentro de los límites establecidos. Siendo el límite inferior 0 y el superior tamaño -1
 
-console.log("%c2.-Leer o recuperar los datos d un arreglo",style_console);
-console.log(`El primer mes del año es: ${mesesAnio[0]}`);
-console.log(`El ultimo mes del año es${mesesAnio[-1]}`);
- console.log(`El treceavo mes del año es: ${mesesAnio[12]}`);
-
- console.log(`¿Qué tipo de dato es un arreglo?:${typeof(mesesAnio)}`);
-
- //Al ser JavaScript un lenguaje debilmente tipado podemos crear arreglos mixtos de tipos de datos
-
- const saludar=function(nombre){return`hola${nombre}!`}
-
- let arrgeloMixto=["String",5,45.26,-200,-.16854,Symbol("Josu"),'z', false, BigInt(11111111222222234523456837564516516514651616816816516165986517657847),{latitud:"20° 18' 0\"N", longuitud:"97° 58' 00\W",altitud:796},saludar,null]
-
- console.log(arrgeloMixto);
- console.table(arrgeloMixto);
- console.log(typeof(arrgeloMixto));
-
- console,log("Verificamos los tipos de datos de los elementos del arreglo: ");
- console.log(`El dato en la posición [0]=${arrgeloMixto[0]} y es del tipo ${typeof(arrgeloMixto[0])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[1]} y es del tipo ${typeof(arrgeloMixto[1])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[2]} y es del tipo ${typeof(arrgeloMixto[2])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[3]} y es del tipo ${typeof(arrgeloMixto[3])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[4]} y es del tipo ${typeof(arrgeloMixto[4])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[5],toString} y es del tipo ${typeof(arrgeloMixto[5])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[6]} y es del tipo ${typeof(arrgeloMixto[6])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[7]} y es del tipo ${typeof(arrgeloMixto[7])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[8]} y es del tipo ${typeof(arrgeloMixto[8])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[9]} y es del tipo ${typeof(arrgeloMixto[9])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[10]} y es del tipo ${typeof(arrgeloMixto[10])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[11]} y es del tipo ${typeof(arrgeloMixto[11])}`);
- console.log(`El dato en la posición [0]=${arrgeloMixto[12]} y es del tipo ${typeof(arrgeloMixto[12])}`);
+//Aceder a los valores de un arreglo de datos para acceder el valor solo bast
 
 
- console.log("%c3.- Arreglod Multidimensionales (Matrices)",style_console);
+console.log("%c2.- Leer o recuperar los datos de un arreglo", style_console);
 
- //Una matriz es una estructura de datos multidimensional (Tabla) de n columnas por m filas
+console.log(`El primer mes del año es: ${mesesAnio[0]} `)
+console.log(`El ultimo mes del año es: ${mesesAnio[mesesAnio,length-1]}`)
 
- let matriz=[[1,2,3,4],['a','b','c','d']]
-//esta es una matriz regular de 4x4
+//No se pueden acceder a elementos con posiciones negativas o posiciones superiores al tamaño denido.
 
-console.log(matriz);
-console.table(matriz);
+console.log(`El mes del año en la posición -1 es: ${mesesAnio[-1]}`)
 
-console.log("Declarando una matriz irregular");
+console.log(`El mes del año en la posición es: ${mesesAnio[12]}`)
 
-const matrizIrregular=[["juan","Pedro","María","Inés"],[true,false,null],[9.2]]
+console.log(`¿Qué tipo de dato es un arreglo? ${typeof(mesesAnio)}`)
 
-console.log(matrizIrregular);
-console.table(matrizIrregular);
+//Al ser JavaScript un lenguaje debilmente tipado podremos crear arreglos mixtos de tipos de Datos
 
-//Accediendo a los valores de una matríz
+console.log("Declarar un arreglo mixto")
+const saludar= function(nombre){return `Hola, ${nombre}!`}
 
-console.log("Leyendo las propiedades de una matriz regular");
-console.log(`Elemento en la posición [0][0]: ${matriz[0][0]}`);
-console.log(`Elemento en la posición [0][0]: ${matriz[0][0]}`);
+let arregloMixto=["String", 9, 45.26,-200,-.16853,Symbol("TANIA"), 'z', false, BigInt(1111112222222333334444444555566666777), true, {latitud:"20°18'0\" N" , longitud: "97°58'00\" W" , altitud:796}, saludar, null]
 
-console.log("Leyendo las propiedades de una matriz irregular");
-console.log(`Elemento en la posición [0][0]: ${matrizIrregular[0][0]}`);
-console.log(`Elemento en la posición [2][0]: ${matrizIrregular[2][0]}`);
-console.log(`Elemento en la posición [1][2]: ${matrizIrregular[1][2]}`);
+console.log(arregloMixto)
+console.table(arregloMixto)
+console.log(typeof(arregloMixto))
 
-//Funciones y Métodos de Arreglos
-//Los métodos de un objeto siempre son invocados usando un . y al termino se delimitan los parametros entre (), en caso de que no lleve (), no es un métoso sino una propiedad
 
-console.log("%c4.-Funciones o Métodos de los Arreglos (Array Methos or Array Functins)",style_console);
-console.log("¿Cómo saber cual es el tamaño de un arreglo?");
-console.log(`meses es un arreglo de tamaño: ${mesesAnio.length}`);
-console.log(`arregloMixto es un arreglo de tamaño: ${arrgeloMixto.length}`);
-console.log("¿Qué pasa con los multidimensionales?");
-console.log(`arreglo es un arreglo de tamaño: ${matriz.length}`);
-console.log("mmm, eso me da l tamaño de  de numero de filas de la matriz, pero como saber el númeo de columnas");
-console.log(`la matriz regular tiene un número de ${matriz[0].length}`);
-console.log("¿Y para las irregulares?");
-//para saber la dimensión de una matriz irregular podemos hacer el uso de ciclo
-let numeroFilas=matrizIrregular.length;
-for (let i=0;i<numeroFilas;i++){
-    console.log(`La longitud de la fila ${[i]} es =${matrizIrregular,[i].length}`);
-}
+console.log("Verificamos los tipos de datos de los elementos del arreglo: ")
+
+console.log(`El dato en la posición[0]= ${arregloMixto[0]} y es del tipo: ${(typeof(arregloMixto[0]))}`)
+console.log(`El dato en la posición[1]= ${arregloMixto[1]} y es del tipo: ${(typeof(arregloMixto[1]))}`)
+console.log(`El dato en la posición[2]= ${arregloMixto[2]} y es del tipo: ${(typeof(arregloMixto[2]))}`)
+console.log(`El dato en la posición[3]= ${arregloMixto[3]} y es del tipo: ${(typeof(arregloMixto[3]))}`)
+console.log(`El dato en la posición[4]= ${arregloMixto[4]} y es del tipo: ${(typeof(arregloMixto[4]))}`)
+console.log(`El dato en la posición[5]= ${arregloMixto[5].toString} y es del tipo: ${(typeof(arregloMixto[5]))}`)
+console.log(`El dato en la posición[6]= ${arregloMixto[6]} y es del tipo: ${(typeof(arregloMixto[6]))}`)
+console.log(`El dato en la posición[7]= ${arregloMixto[7]} y es del tipo: ${(typeof(arregloMixto[7]))}`)
+console.log(`El dato en la posición[8]= ${arregloMixto[8]} y es del tipo: ${(typeof(arregloMixto[8]))}`)
+console.log(`El dato en la posición[9]= ${arregloMixto[9]} y es del tipo: ${(typeof(arregloMixto[9]))}`)
+console.log(`El dato en la posición[10]= ${arregloMixto[10]} y es del tipo: ${(typeof(arregloMixto[10]))}`)
+console.log(`El dato en la posición[11]= ${arregloMixto[11]} y es del tipo: ${(typeof(arregloMixto[11]))}`)
+console.log(`El dato en la posición[12]= ${arregloMixto[12]} y es del tipo: ${(typeof(arregloMixto[12]))}`)
+
+console.log("%c3.- Arreglos Multidimensionales (Matrices)", style_console);
+
+console.log("Declarando una matriz regular")
+//Una matriz es uuna estructura de datos multidimensional (tabla) de n columnas o m filas
+
+let matriz=[[1,2,3,4], ['a','b','c','d']]
+
+//Este es una matriz regular de 4x4
+
+console.log(matriz)
+console.table(matriz)
+
+
+console.log("Declaramos una matriz irregular")
+
+const matrizIrregular=[["Juan", "Pedro", "Maria", "Inés"], [true, false, null], [9.2]]
+
+console.log(matrizIrregular)
+console.table(matrizIrregular)
+
+//Accediendo a los valores de una matriz
+
+console.log("Leyendo las Propiedades de una Matriz Regular")
+
+console.log(`Elementos en la posición [0][0]: ${matriz[0][0]}`)
+console.log(`Elementos en la posición [1][2]: ${matriz[1][2]}`)
+
+//Accediendo a los valores de la matriz irregular
+
+console.log("Leyendo las Propiedades de una Matriz Irregular")
+
+console.log(`Elementos en la posición [0][0]: ${matrizIrregular[0][0]}`)
+console.log(`Elementos en la posición [1][2] (¿Es Maria mayor?): ${matrizIrregular[1][2]}`)
+console.log(`Elementos en la posición [2][0] (Promedio de Juan): ${matrizIrregular[2][0]}`)
+
+//Funciones y métodos de Arreglos
+//los metodos de ubn objeto siempre son invocados usando un . y su termino se delimitan los parametros entre(), en caso de que no lleve(), no es un metodo, sino una propiedad.
+
+//
+console.log("%c4.- Funciones o Métodos de los Arreglos (Array Methos or Array functions)", style_console)
+console.log("¿cómo sabemos cuál es el tamaño de un arreglo?")
+console.log(`meses es un arreglo de tamaño: ${mesesAnio.length}`)
+console.log(`ArregloMixto es un arreglo de tamaño: ${arregloMixto.length}`)
+
+console.log("¿Qué pasa con los multidimensionales?")
+console.log(`matriz es un arreglo de tamaño: ${matriz.length}`)
+console.log("mmm, eso me da el tamaño de numero de las filas de la matriz, pero cómo saber el número de columna?")
+console.log(`matriz regular tiene un numero de ${matriz[0].length}`)
+
+
+console.log(`Y para la matrizIrregular?`)
+
+//Para saber la dimensión de una matriz irregular podemos hacer el uso del ciclo
+
+let numeroFilas=matrizIrregular.length
+for(let i = 0; i < numeroFilas; i++)
+    console.log(`La longitud de la fila ${[i]} es = ${matrizIrregular[i].length}`)
