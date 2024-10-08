f2d74169ef0a4da0e48d6cfe91b928cbdb6d9433
Yáred Amaury Romero <230190@utxicotepec.edu.mx>
2024-10-08 14:28:07 -0600

    Práctica 07
    Explicación y uso de IF,CASE y OT

--- JS/ejercicio05.js
+++ JS/ejercicio05.js
@@ -1,5 +1,5 @@
 //Estilización de los mensajes de salida
-const bg = "linear-gradient(11deg, rgba(199,0,36,1) 0%, rgba(255,129,26,1) 33%, rgba(255,191,26,1) 86%)";
+const bg = "linear-gradient(11deg, rgba(129,50,36,1) 0%, rgba(155,129,26,1) 23%, rgba(255,191,26,1) 86%)";
 const style_console = `background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold`
 
 //Personalización de las Salidas a Consola
@@ -7,7 +7,7 @@ console.warn("Prática 07: Arreglos en Java Script")
 
 console.log("%c1.- Sí/Entonces ... (IF)", style_console);
 //Le indica al programa que hacer o que no en base a una prueba lógica de verdadero o falso
-let fechaActual = new Date();
+let fechaActual = new Date(2024,1,6);
 console.log(`Hola, la fecha de hoy es ${fechaActual.toString()}`);
 
 //y si la necesitamos en formato local?
@@ -45,8 +45,69 @@ let inicioInvierno= new Date(anio, 11, 21);
 
 let horarioVerano = false;
 
-if(fechaActual >= inicioPrimavera || fechaActual<= inicioVerano)
+if(fechaActual >= inicioPrimavera && fechaActual<= inicioVerano)
 {
-    console.log("Estamos en PRIMAVERA...")
+    console.log("Estamos en PRIMAVERA...");
+    console.log("Inicia la floración de muchas plantas");
+console.log("Los dias son más largos y las noches más cortas");
+console.log("Muchos animales despiertan de la invernación");
+console.log();
+estacion="Primavera";
+horarioVerano=true;
+} 
+else if(fechaActual >= inicioVerano && fechaActual< inicioOtonio)
+{
+    console.log("Estamos en OTOÑO...");
+    console.log("Los arboles suelen cambiar de follaje");
+console.log("Se registran temperaturas más templadas");
+console.log("Los animales comienzan con la recolección de alimeto y preparan sus espacios para la hibernación, incluso algunas aves migran");
+estacion="Verano";
+horarioVerano=true;
 
+}else{
+
+    console.log("Estamos en INVIERNO...");
+    console.log("En esta temporada los dias son más cortos y las noches más largas");
+console.log("En algunas regiones suele nevar");
+console.log("Dado las bajas tempersturas, se recomienda abrigarse");
+estacion="Invierno";
+horarioVerano=true;
 }
+
+console.log("%c2.- Operdaor Ternario (validación?cumple.no_cumple)", style_console);
+//En javaScript existia una operación simplificada que valida si una condición se cumple o no, y que hacer en cada caso
+
+const edadPersona=18;
+const mayorEdadMX=18;
+const mayorEdadUS=21;
+let evaluarMayoriaEdad=(edad)=> edad>18 ?"Eres Mayor de edad":"No eres mayor de edad"
+
+console.log("Evaluando la mayoria de edad en una persona...");
+console.log(evaluarMayoriaEdad(edadPersona));
+
+//Sin embargo tenemos que considerar que la mayoría de edad varia en cada país por cuestines legales, por lo que debemos considerar un segundo parámetro en la evaluación.
+evaluarMayoriaEdad=(edad,pais)=>(edad>=18 && pais==="MX")?`En ${pais} No eres mayor de edad`:`En ${pais}No eres mayor de edad`;
+console.log("Evaluando la mayoria de edad en una persona en México...");
+console.log(evaluarMayoriaEdad(edadPersona,"MX"));
+console.log("Evaluando la mayoria de edad en una persona en USA...");
+console.log(evaluarMayoriaEdad(edadPersona,"US"));
+
+console.log("%c3.- SWITCH- CASE (elección por valor definido)", style_console);
+//calculando tu generación en base a tu edad
+let asignaGeneracion=(anioNacimiento)=>{
+anioNacimiento=2003;
+switch(true){
+case(anioNacimiento<1968):
+    return "Baby Boomers";
+
+    case(anioNacimiento=1968 && anioNacimiento<=1980):
+    return "Generación X";
+
+    case(anioNacimiento=1981 && anioNacimiento<=1993):
+    return "Generación Y";
+
+    case(anioNacimiento=1994 && anioNacimiento<=2010):
+    return "Generación Z";
+}
+}
+console.log(`Dado que nació en el año 1997 soy de la generación ${asignaGeneracion(1982)}`);
